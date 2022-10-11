7aced9c05ec8bf29b6d028a9c7502c582219df59
hitman18@student.ubc.ca <hitman18@student.ubc.ca>
2022-10-10 23:13:24 -0700

    Added point mesh distance function

--- tutorial/609_Boolean/main.cpp
+++ tutorial/609_Boolean/main.cpp
@@ -1,5 +1,6 @@
 #include <igl/readOFF.h>
 #include <igl/writeSTL.h>
+#include <igl/point_mesh_squared_distance.h>
 //#undef IGL_STATIC_LIBRARY
 #include <igl/copyleft/cgal/mesh_boolean.h>
 #include <igl/opengl/glfw/Viewer.h>
@@ -8,9 +9,41 @@
 #include <iostream>
 #include <string>
 #include <math.h> 
+#include <vector>
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Surface_mesh.h>
+#include <CGAL/Polygon_mesh_processing/remesh.h>
+#include <CGAL/Polygon_mesh_processing/border.h>
+#include <CGAL/Mesh_3/dihedral_angle_3.h>
+#include <CGAL/Polygon_mesh_processing/IO/polygon_mesh_io.h>
+#include <boost/iterator/function_output_iterator.hpp>
+
+
+
 using namespace Eigen;
 using namespace std;
 
+// CGAL Remeshing declaration
+typedef CGAL::Exact_predicates_inexact_constructions_kernel   K;
+typedef CGAL::Surface_mesh<K::Point_3>                        Mesh;
+typedef boost::graph_traits<Mesh>::halfedge_descriptor        halfedge_descriptor;
+typedef boost::graph_traits<Mesh>::edge_descriptor            edge_descriptor;
+namespace PMP = CGAL::Polygon_mesh_processing;
+
+struct halfedge2edge
+{
+    halfedge2edge(const Mesh& m, std::vector<edge_descriptor>& edges)
+        : m_mesh(m), m_edges(edges)
+    {}
+    void operator()(const halfedge_descriptor& h) const
+    {
+        m_edges.push_back(edge(h, m_mesh));
+    }
+    const Mesh& m_mesh;
+    std::vector<edge_descriptor>& m_edges;
+};
+// CGAL Remeshing declaration
 
 Eigen::MatrixXd VA,VB,VC;
 Eigen::VectorXi J,I;
@@ -167,7 +200,36 @@ void generate_meshB() {
     double A_SIZE = 200;
     double GAP_DISTANCE = 0;
     double Volume_A = pow(A_SIZE, 3);
-    std::cout << "Please enter the radius of material B spheres and press ENTER: ";
+
+    int exit_loop = 0;
+    std::cout << "Select material B shape (1 for sphere,  2 for cylinder, 3 for cube): ";
+    while (exit_loop == 0) {
+        int B_TYPE;
+        cin >> B_TYPE;
+        switch (B_TYPE)
+        {
+        default:
+            std::cout << "Wrong number! Please enter a VALID material B type: ";
+            break;
+        case 1:
+            std::cout << "Sphere shape selected!" << std::endl;
+            igl::read_triangle_mesh("C:/Users/Derek Zhang/Desktop/WAAM Research/CAD/zero_ball.stl", v1, f1);
+            exit_loop = 1;
+            break;
+        case 2:
+            std::cout << "Cylinder shape selected!" << std::endl;
+            igl::read_triangle_mesh("C:/Users/Derek Zhang/Desktop/WAAM Research/CAD/zero_cylinder.stl", v1, f1);
+            exit_loop = 1;
+            break;
+        case 3:
+            std::cout << "Cube shape selected!" << std::endl;
+            igl::read_triangle_mesh("C:/Users/Derek Zhang/Desktop/WAAM Research/CAD/zero_cube.stl", v1, f1);
+            exit_loop = 1;
+            break;
+        }
+    }
+    
+    std::cout << "Please enter the size of material B and press ENTER (default 40): ";
     cin >> BALL_SIZE;
     std::cout << "Please enter the gap distance between material B spheres and press ENTER: ";
     cin >> GAP_DISTANCE;
@@ -182,7 +244,6 @@ void generate_meshB() {
     std::cout << "Generating......" << endl;
 
     B_Number = 32; // set it to 32 for demo;
-    igl::read_triangle_mesh("C:/Users/Derek Zhang/Desktop/WAAM Research/CAD/zero_ball.stl", v1, f1);
     scale_mesh(v1, BALL_SIZE / 40.0); // v1 will be modified
 
     // Create material B meshes now
@@ -194,41 +255,41 @@ void generate_meshB() {
     //Eigen::MatrixXd v_tmp2 = move_mesh(v1, -50, -50, -50);
     //std::tuple<MatrixXd, MatrixXi> mesh_matrix = connect_meshes(v_tmp1, v_tmp2, f1, f1);
 
-    for (int i = 0; i < 6; ++i) {
-        for (int j = 0; j < 6; ++j) {
-            Eigen::MatrixXd v_tmp1 = move_mesh(v1, space[i], space[j], A_SIZE * 0.5);
-            Eigen::MatrixXd v_tmp2 = move_mesh(v1, space[j], space[i], A_SIZE * 0.5);
-            std::tuple<MatrixXd, MatrixXi> mesh_matrix = connect_meshes(v_tmp1, v_tmp2, f1, f1);
-            meshB.push_back(mesh_matrix);
-        }
-    }
-
-    for (int i = 0; i < 6; ++i) {
-        for (int j = 0; j < 6; ++j) {
-            Eigen::MatrixXd v_tmp1 = move_mesh(v1, space[i], space[j], A_SIZE * 0.3);
-            Eigen::MatrixXd v_tmp2 = move_mesh(v1, space[j], space[i], A_SIZE * 0.3);
-            std::tuple<MatrixXd, MatrixXi> mesh_matrix = connect_meshes(v_tmp1, v_tmp2, f1, f1);
-            meshB.push_back(mesh_matrix);
-        }
-    }
-
-    for (int i = 0; i < 6; ++i) {
-        for (int j = 0; j < 6; ++j) {
-            Eigen::MatrixXd v_tmp1 = move_mesh(v1, space[i], space[j], A_SIZE * -0.15);
-            Eigen::MatrixXd v_tmp2 = move_mesh(v1, space[j], space[i], A_SIZE * -0.15);
-            std::tuple<MatrixXd, MatrixXi> mesh_matrix = connect_meshes(v_tmp1, v_tmp2, f1, f1);
-            meshB.push_back(mesh_matrix);
-        }
-    }
-
-    // // Ring shape pattern
-    //int num = 10, radius = 100;
-    //for (int i = 0; i < num; ++i) {
-    //    Eigen::MatrixXd v_tmp1 = move_mesh(v1, radius * cos(2 * (3.1416) / num * i), radius * sin(2 * (3.1416) / num * i), 0);
-    //    Eigen::MatrixXd v_tmp2 = move_mesh(v1, radius * cos(2 * (3.1416) / num * i), radius * sin(2 * (3.1416) / num * i), A_SIZE * -0.3);
-    //    std::tuple<MatrixXd, MatrixXi> mesh_matrix = connect_meshes(v_tmp1, v_tmp2, f1, f1);
-    //    meshB.push_back(mesh_matrix);
+    //for (int i = 0; i < 6; ++i) {
+    //    for (int j = 0; j < 6; ++j) {
+    //        Eigen::MatrixXd v_tmp1 = move_mesh(v1, space[i], space[j], A_SIZE * 0.5);
+    //        Eigen::MatrixXd v_tmp2 = move_mesh(v1, space[j], space[i], A_SIZE * 0.5);
+    //        std::tuple<MatrixXd, MatrixXi> mesh_matrix = connect_meshes(v_tmp1, v_tmp2, f1, f1);
+    //        meshB.push_back(mesh_matrix);
+    //    }
     //}
+
+    //for (int i = 0; i < 6; ++i) {
+    //    for (int j = 0; j < 6; ++j) {
+    //        Eigen::MatrixXd v_tmp1 = move_mesh(v1, space[i], space[j], A_SIZE * 0.3);
+    //        Eigen::MatrixXd v_tmp2 = move_mesh(v1, space[j], space[i], A_SIZE * 0.3);
+    //        std::tuple<MatrixXd, MatrixXi> mesh_matrix = connect_meshes(v_tmp1, v_tmp2, f1, f1);
+    //        meshB.push_back(mesh_matrix);
+    //    }
+    //}
+
+    //for (int i = 0; i < 6; ++i) {
+    //    for (int j = 0; j < 6; ++j) {
+    //        Eigen::MatrixXd v_tmp1 = move_mesh(v1, space[i], space[j], A_SIZE * -0.15);
+    //        Eigen::MatrixXd v_tmp2 = move_mesh(v1, space[j], space[i], A_SIZE * -0.15);
+    //        std::tuple<MatrixXd, MatrixXi> mesh_matrix = connect_meshes(v_tmp1, v_tmp2, f1, f1);
+    //        meshB.push_back(mesh_matrix);
+    //    }
+    //}
+
+     // Ring shape pattern
+    int num = 10, radius = 100;
+    for (int i = 0; i < num; ++i) {
+        Eigen::MatrixXd v_tmp1 = move_mesh(v1, radius * cos(2 * (3.1416) / num * i), radius * sin(2 * (3.1416) / num * i), 0);
+        Eigen::MatrixXd v_tmp2 = move_mesh(v1, radius * cos(2 * (3.1416) / num * i), radius * sin(2 * (3.1416) / num * i), A_SIZE * -0.3);
+        std::tuple<MatrixXd, MatrixXi> mesh_matrix = connect_meshes(v_tmp1, v_tmp2, f1, f1);
+        meshB.push_back(mesh_matrix);
+    }
 
     std::tuple<MatrixXd, MatrixXi> tmp1 = meshB[0];
     std::tuple<MatrixXd, MatrixXi> tmp2;
@@ -239,7 +300,7 @@ void generate_meshB() {
         tmp1 = tmp2;
     }
 
-    igl::writeSTL("C:/Users/Derek Zhang/Desktop/WAAM Research/CAD/materialB/meshB_large_cube.stl", get<0>(tmp1), get<1>(tmp1));
+    igl::writeSTL("C:/Users/Derek Zhang/Desktop/WAAM Research/CAD/materialB/meshB_ring_cylinder.stl", get<0>(tmp1), get<1>(tmp1));
 }
 
 void combine_gcode() {
@@ -300,20 +361,84 @@ void combine_gcode() {
 
 }
 
+void CGAL_remesh() {
+    const std::string filename = "C:/Users/Derek Zhang/Desktop/WAAM Research/CAD/Shell/testBOOLEAN.STL";
+    Mesh mesh;
+    if (!PMP::IO::read_polygon_mesh(filename, mesh) || !CGAL::is_triangle_mesh(mesh))
+    {
+        std::cerr << "Invalid input." << std::endl;
+        return;
+    }
+    double target_edge_length = 3.0;
+    unsigned int nb_iter = 10;
+    std::cout << "Split border...";
+    std::vector<edge_descriptor> border;
+    PMP::border_halfedges(faces(mesh), mesh, boost::make_function_output_iterator(halfedge2edge(mesh, border)));
+    PMP::split_long_edges(border, target_edge_length, mesh);
+    std::cout << "done." << std::endl;
+    std::cout << "Start remeshing of " << filename
+        << " (" << num_faces(mesh) << " faces)..." << std::endl;
+
+    // give each vertex a name, the default is empty
+    Mesh::Property_map<edge_descriptor, bool> is_constrained =
+        mesh.add_property_map<edge_descriptor, bool>("e:is_constrained", false).first;
+
+    //detect sharp features
+    BOOST_FOREACH(edge_descriptor e, edges(mesh))
+    {
+        halfedge_descriptor hd = halfedge(e, mesh);
+        if (!is_border(e, mesh)) {
+            double angle = CGAL::Mesh_3::dihedral_angle(mesh.point(source(hd, mesh)),
+                mesh.point(target(hd, mesh)),
+                mesh.point(target(next(hd, mesh), mesh)),
+                mesh.point(target(next(opposite(hd, mesh), mesh), mesh)));
+            if (CGAL::abs(angle) < 100)
+                is_constrained[e] = true;
+        }
+    }
+
+    PMP::isotropic_remeshing(faces(mesh), target_edge_length, mesh,
+        CGAL::parameters::number_of_iterations(nb_iter)
+        .edge_is_constrained_map(is_constrained)); //i.e. protect border, here
+
+    CGAL::IO::write_STL("C:/Users/Derek Zhang/Desktop/WAAM Research/CAD/Shell/remesh.stl", mesh);
+
+}
+
+
 int main(int argc, char *argv[])
 {
-    //test git push
     Eigen::MatrixXd v1, v2, v3;
     Eigen::MatrixXi f1, f2, f3;
 
-    igl::readOFF("C:/Users/Derek Zhang/Desktop/WAAM Research/CAD/Shell/Cube-shell.off",v1,f1);
-    igl::readOFF("C:/Users/Derek Zhang/Desktop/WAAM Research/CAD/Shell/meshB_large_circle.off",v2,f2);
+    //CGAL_remesh();
+    //generate_meshB();
 
-    igl::copyleft::cgal::mesh_boolean(v1, f1, v2, f2, igl::MESH_BOOLEAN_TYPE_MINUS, v3, f3);
-    igl::writeSTL("C:/Users/Derek Zhang/Desktop/WAAM Research/CAD/Shell/testBOOLEAN.STL", v3, f3);
+    //igl::readOFF("C:/Users/Derek Zhang/Desktop/WAAM Research/CAD/Shell/Cube-shell.off",v1,f1);
+    //igl::readOFF("C:/Users/Derek Zhang/Desktop/WAAM Research/CAD/Shell/meshB_large_circle.off",v2,f2);
 
-    //igl::read_triangle_mesh("C:/Users/Derek Zhang/Desktop/WAAM Research/CAD/Cube.STL", v1, f1);
-    //igl::writeOFF("C:/Users/Derek Zhang/Desktop/WAAM Research/CAD/Cube-test.off", v1, f1);
+    //igl::copyleft::cgal::mesh_boolean(v1, f1, v2, f2, igl::MESH_BOOLEAN_TYPE_MINUS, v3, f3);
+    //igl::writeSTL("C:/Users/Derek Zhang/Desktop/WAAM Research/CAD/Shell/testBOOLEAN.STL", v3, f3);
+
+    igl::read_triangle_mesh("C:/Users/Derek Zhang/Desktop/WAAM Research/CAD/Cube.STL", v1, f1);
+    igl::read_triangle_mesh("C:/Users/Derek Zhang/Desktop/WAAM Research/CAD/zero_ball.STL", v2, f2);
+    v2 = move_mesh(v2, 0, 0, 60);
+    Matrix<double, Dynamic, 1> d;
+    Matrix<int, Dynamic, 1> I;
+    Matrix<double, Dynamic, 3> C;
+
+    // Compute distances from a set of points P to a triangle mesh (V,F)
+      // Inputs:
+      //   P  #P by 3 list of query point positions
+      //   V  #V by 3 list of vertex positions
+      //   F  #F by 3 list of triangle indices
+      // Outputs:
+      //   sqrD  #P list of smallest squared distances
+      //   I  #P list of facet indices corresponding to smallest distances
+      //   C  #P by 3 list of closest points
+    igl::point_mesh_squared_distance(v2, v1, f1, d, I, C);
+    cout << "distance: " << endl;
+    for (auto ele : d) cout << ele << endl;
 
 
     /*igl::opengl::glfw::Viewer viewer;
